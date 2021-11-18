676d1c3ee26822c6a11053fefdba4fac6f38c534
Meniem <meniem.hany58@yahoo.com>
2021-11-18 10:46:47 +0200

    Final

---
+++ cmake-build-debug/CMakeFiles/Chess_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess_Project.dir/Chess_Project_main.c.obj"
+  "Chess_Project.exe"
+  "Chess_Project.exe.manifest"
+  "Chess_Project.pdb"
+  "libChess_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chess_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
